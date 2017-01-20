ae9799c7293d44d6897a0aedf4ca9565b0626c38
Peter Thompson <peter.thompson@radimetrics.com>
2017-01-20 09:59:44 -0500

    dataset sems to load ok

--- run_analysis.R
+++ run_analysis.R
@@ -4,45 +4,47 @@ loadRawData<-function()
 {
 	# load all of the raw data into one big dataset.
 	#column names for most things
-	print('loading data')
+	message('loading data')
 	
 	#features<-readLines('./UCI HAR Dataset/features.txt')
 	features<-read.table('./UCI HAR Dataset/features.txt',row.names=1,sep=' ',as.is=c(TRUE,TRUE))
-	print('read features')
+	message('read features')
 	
 	
 	# train measurements
-	print('reading train measurements')
+	message('reading train measurements')
 	trainMeasures<-read.table('./UCI HAR Dataset/train/X_train.txt')#,col.names=features[[1]])
 	# test measurements
-	print('reading test measurements')
+	message('reading test measurements')
 	testMeasures<-read.table('./UCI HAR Dataset/test/X_test.txt')#,col.names=features[[1]])
 		
 	# do train then test for all things, otherwise oredering will get screwed up
-	print('merging train and test measurements')
+	message('merging train and test measurements')
 	totalMeasures<-rbind(trainMeasures,testMeasures)
 	
 	
 	# subject ids
-	print('reading train subjects')
-	trainSubjects<-readLines('./UCI HAR Dataset/train/subject_train.txt')
-	print('reading test subjects')
-	testSubjects<-readLines('./UCI HAR Dataset/test/subject_test.txt')
+	message('reading train subjects')
+	trainSubjects<-read.table('./UCI HAR Dataset/train/subject_train.txt')
+	message('reading test subjects')
+	testSubjects<-read.table('./UCI HAR Dataset/test/subject_test.txt')
 	# merge subjects
-	print('merging train and test subjects')
+	message('merging train and test subjects')
 	totalSubjects<-rbind(trainSubjects,testSubjects)
 	
 	# Activities
-	print('reading train activities')
-	trainActivity<-readLines('./UCI HAR Dataset/train/y_train.txt')
-	print('reading test activities')
-	testActivity<-readLines('./UCI HAR Dataset/test/y_test.txt')
+	message('reading train activities')
+	trainActivity<-read.table('./UCI HAR Dataset/train/y_train.txt')
+	message('reading test activities')
+	testActivity<-read.table('./UCI HAR Dataset/test/y_test.txt')
 	
-	print('merging train and test activities')
+	message('merging train and test activities')
 	totalActivity<-rbind(trainActivity,testActivity)
 	
+	
 	# have all the columns now. Merge rows into one big dataframe
-	Dataset<-cbind(totalSubject,totalActivity,totalMeasures)
+	Dataset<-cbind(totalSubjects,totalActivity,totalMeasures)
+	names(Dataset)<-c('subject','activity',features[[1]])
 	Dataset
 	
 	
