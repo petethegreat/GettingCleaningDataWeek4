c77672e4dfbc43cca1853a933665f9423884f3b9
Peter Thompson <peter.thompson@radimetrics.com>
2017-01-24 20:46:52 -0500

    names not sorted

--- run_analysis.R
+++ run_analysis.R
@@ -12,6 +12,93 @@ main<-function()
 cleanData<-function(theRawData)
 {
 	#
+	features<-names(theRawData)
+	meanNames<-grep('mean()',features[[1]],fixed=TRUE,value=TRUE)
+	stdNames<-grep('std()',features[[1]],fixed=TRUE,value=TRUE)
+	
+	descMeanNames<-gsub('-mean()','',meanNames,fixed=TRUE)
+	descMeanNames<-gsub('^t','TimeSeriesMean',descMeanNames)
+	descMeanNames<-gsub('Acc','Acceleration',descMeanNames,fixed=TRUE)
+	descMeanNames<-gsub('-([XYZ])$','\0Component',descMeanNames) - borked
+	
+	
+	
+	
+	# count 33 means and 33 stds - that's 66 variables of interest.
+	# also want activity and subject - thats 68 variables in total.
+	# meanFreq - mean frequency of the measurement. This is (I think) something we don't want.
+	# means:
+	# tBodyAcc-mean()-X
+	# tBodyAcc-mean()-Y
+	# tBodyAcc-mean()-Z
+	# tGravityAcc-mean()-X
+	# tGravityAcc-mean()-Y
+	# tGravityAcc-mean()-Z
+	# tBodyAccJerk-mean()-X
+	# tBodyAccJerk-mean()-Y
+	# tBodyAccJerk-mean()-Z
+	# tBodyGyro-mean()-X
+	# tBodyGyro-mean()-Y
+	# tBodyGyro-mean()-Z
+	# tBodyGyroJerk-mean()-X
+	# tBodyGyroJerk-mean()-Y
+	# tBodyGyroJerk-mean()-Z
+	# tBodyAccMag-mean()
+	# tGravityAccMag-mean()
+	# tBodyAccJerkMag-mean()
+	# tBodyGyroMag-mean()
+	# tBodyGyroJerkMag-mean()
+	# fBodyAcc-mean()-X
+	# fBodyAcc-mean()-Y
+	# fBodyAcc-mean()-Z
+	# fBodyAccJerk-mean()-X
+	# fBodyAccJerk-mean()-Y
+	# fBodyAccJerk-mean()-Z
+	# fBodyGyro-mean()-X
+	# fBodyGyro-mean()-Y
+	# fBodyGyro-mean()-Z
+	# fBodyAccMag-mean()
+	# fBodyBodyAccJerkMag-mean()
+	# fBodyBodyGyroMag-mean()
+	# fBodyBodyGyroJerkMag-mean()
+	
+	# standard deviations:
+	# tBodyAcc-std()-X
+	# tBodyAcc-std()-Y
+	# tBodyAcc-std()-Z
+	# tGravityAcc-std()-X
+	# tGravityAcc-std()-Y
+	# tGravityAcc-std()-Z
+	# tBodyAccJerk-std()-X
+	# tBodyAccJerk-std()-Y
+	# tBodyAccJerk-std()-Z
+	# tBodyGyro-std()-X
+	# tBodyGyro-std()-Y
+	# tBodyGyro-std()-Z
+	# tBodyGyroJerk-std()-X
+	# tBodyGyroJerk-std()-Y
+	# tBodyGyroJerk-std()-Z
+	# tBodyAccMag-std()
+	# tGravityAccMag-std()
+	# tBodyAccJerkMag-std()
+	# tBodyGyroMag-std()
+	# tBodyGyroJerkMag-std()
+	# fBodyAcc-std()-X
+	# fBodyAcc-std()-Y
+	# fBodyAcc-std()-Z
+	# fBodyAccJerk-std()-X
+	# fBodyAccJerk-std()-Y
+	# fBodyAccJerk-std()-Z
+	# fBodyGyro-std()-X
+	# fBodyGyro-std()-Y
+	# fBodyGyro-std()-Z
+	# fBodyAccMag-std()
+	# fBodyBodyAccJerkMag-std()
+	# fBodyBodyGyroMag-std()
+	# fBodyBodyGyroJerkMag-std()
+	
+	
+	
 	return(theRawData)
 	
 }
